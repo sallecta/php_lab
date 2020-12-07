c8476fd0f7d3e26735dfe8b1b071f9f542e0e14b
sallecta <sallecta@inbox.ru>
2020-12-07 05:35:04 +0300

    ...

---
+++ samples/db_sqlite.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>db_sqlite.php</title>
+ </head>
+ <body>
+    <h1>DB SQLite</h1>
+    <p> var_dump(SQLite3::version()): <?php var_dump(SQLite3::version()); ?></p>
+    <p> SQLite3::version()["versionString"]): <?php echo(SQLite3::version()["versionString"]); ?></p>
+ 
+ </body>
+</html>
